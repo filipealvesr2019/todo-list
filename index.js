a748f2cb30302988109a181b47619776020c2329
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-27 15:52:10 -0300

    feat: adiciona checkbox pra as tarefas.

--- index.js
+++ index.js
@@ -5,12 +5,37 @@ let task = [
 
 ]
 
+const getcheckBoxInput =  ({id, description, checked}) => {
+    const checkbox = document.createElement('input');
+    const label = document.createElement('label');
+    const wrapper = document.createElement('div');
+    const checkboxId = `task ${id}`;
+
+    checkbox.type = 'checkbox';
+    checkbox.id = checkboxId
+    checkbox.checked = checked;
+
+    label.textContent = description;
+    label.htmlFor = checkboxId;
+
+    wrapper.className = 'checkbox-label-container';
+    
+    wrapper.appendChild(checkbox);
+    wrapper.appendChild(label);
+
+    return wrapper
+
+
+}
+
 window.onload = function(){
     task.forEach((task) => {
+        const checkbox = getcheckBoxInput(task);
         const list = document.getElementById('todo-list');
         const toDo = document.createElement('li');
 
-        toDo.textContent = task.description;
+        toDo.id = task.id;
+        toDo.appendChild(checkbox)
 
         list.appendChild(toDo)
     })
